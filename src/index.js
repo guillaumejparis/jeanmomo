185def4b23703f393789fc5338dc11968a7d7d75
Guillaume Paris <guillaumejparis@gmail.com>
2021-12-15 18:08:38 +0100

    Add magnet handling

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+if (window.location.hash && window.location.hash.includes("download")) {
+  window.open("/qb" + window.location.hash, "_blank");
+  history.back();
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
